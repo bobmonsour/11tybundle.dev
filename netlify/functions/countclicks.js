fb7e6805b73a86f9a9111c1f41b53d19c652dd6a
Bob Monsour <bob.monsour@gmail.com>
2023-06-11 21:31:14 -0700

    Fixed uri for db function

--- netlify/functions/countclicks.js
+++ netlify/functions/countclicks.js
@@ -13,7 +13,7 @@ const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
