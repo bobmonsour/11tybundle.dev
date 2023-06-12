57b38797b1fe8b0fec35239d92ac53d26b2c87a5
Bob Monsour <bob.monsour@gmail.com>
2023-06-11 21:10:54 -0700

    Changed to using Firestore with proper auth

--- netlify/functions/countclicks.js
+++ netlify/functions/countclicks.js
@@ -1,57 +1,67 @@
 // /functions/countclicks.js - A Netlify function to count outbound link clicks
 
-// Using a Firebase Realtime Database to store the click counts
-// Each record in the database has an id that is identical to the record id
-// used in the Airtable base. In fact, the Airtable record id is used as the
-// key in the Firebase database. The record has two fields: count and type.
-// Each time a blog post link is clicked, the count is incremented and the
-// type is set to "blog post".
+// Using a Firebase Firestore Database to store the click counts
+// The Firestore database has a collection called posts. In each post document
+// there is a recordID (same as the Airtable record ID). Each record also has a
+// count that will be incremented by 1 on each invocation of this function.
 
-// require("dotenv").config();
-// const { initializeApp } = require("firebase/app");
-// const { getDatabase, ref, runTransaction } = require("firebase/database");
-// var firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.FIREBASE_DATABASE_URL,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-// };
+require("dotenv").config();
+const admin = require("firebase-admin");
 
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// Initialize Realtime Database and get a reference to the service
-// const dbRef = getDatabase(app);
+// setup firebase admin
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 
-exports.handler = async (event, context) => {
-  console.log("netlify function triggered");
-  const querystring = event.queryStringParameters;
-  const recordId = querystring.itemid;
-  const link = querystring.itemlink;
-  // console.log("link: " + link);
-  // const recordRef = ref(dbRef, recordId);
-  // Run a transaction on the record to increment the count and set the type value
-  // runTransaction(recordRef, (currentData) => {
-  //   if (!currentData) {
-  //     // If the record doesn't exist, create it with a count of 1 and the type value
-  //     return { count: 1, type: type };
-  //   } else {
-  //     // If the record exists, increment the count and set the type value
-  //     currentData.count = (currentData.count || 0) + 1;
-  //     currentData.type = type;
-  //     return currentData;
-  //   }
-  // })
-  //   .then(() => {
-  //     console.log("Transaction successful");
-  //   })
-  //   .catch((error) => {
-  //     console.error("Transaction failed: ", error);
-  //   });
-  return {
-    statusCode: 200,
-    body: `${recordId} ${link}`,
-  };
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+const db = admin.firestore();
+
+exports.handler = async function (event, context) {
+  // get the post ID and link from the query string parameters
+  postID = event.queryStringParameters.itemid;
+  postLink = event.queryStringParameters.itemlink;
+  console.log("Netlify Function `countclicks` invoked");
+  console.log(`postID: ${postID}`);
+  console.log(`postLink: ${postLink}`);
+
+  const postRef = db.collection("posts").doc(postID);
+
+  try {
+    await db.runTransaction(async (t) => {
+      const doc = await t.get(postRef);
+
+      let newCount;
+      if (!doc.exists) {
+        newCount = 1;
+      } else {
+        newCount = doc.data().postCount + 1;
+      }
+
+      t.set(postRef, { postCount: newCount }, { merge: true });
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Transaction success" }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: `Transaction failure: ${err.message}` }),
+    };
+  }
 };
