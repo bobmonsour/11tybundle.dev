08dc186f68bb6a8fbfcf5db4202853217e9cf67d
Bob Monsour <bob.monsour@gmail.com>
2024-06-28 12:55:03 -0700

    Simplified filter imports

--- src/_config/filters.js
+++ src/_config/filters.js
@@ -1,15 +1,16 @@
 import { cachedSlugify } from "./filters/cachedslugify.js";
 import { isCurrentPage } from "./filters/iscurrentpage.js";
-import { formatItemDate } from "./filters/formatting.js";
-import { formatPostDate } from "./filters/formatting.js";
-import { formatFirehoseDate } from "./filters/formatting.js";
-import { formatNumber } from "./filters/formatting.js";
+import {
+  formatItemDate,
+  formatPostDate,
+  formatFirehoseDate,
+  formatNumber,
+} from "./filters/formatting.js";
 import { getBundleItems } from "./filters/getbundleitems.js";
 import { getDescription } from "./filters/getdescription.js";
 import { getRSSlink } from "./filters/getrsslink.js";
 import { plainDate } from "./filters/formatting.js";
-import { postCountByAuthor } from "./filters/postsbyauthor.js";
-import { postsByAuthor } from "./filters/postsbyauthor.js";
+import { postCountByAuthor, postsByAuthor } from "./filters/postsbyauthor.js";
 import { postsInCategory } from "./filters/postsincategory.js";
 import { readingTime } from "./filters/readingtime.js";
 import { webmentionsByUrl } from "./filters/webmentionsbyurl.js";
