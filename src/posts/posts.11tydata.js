a9c56f58ee0c22cc339deaba4ada6b01a774c2e5
Bob Monsour <bob.monsour@gmail.com>
2025-07-30 14:54:09 -0700

    Update to alt text for 11y Bundle Blog posts

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,12 +1,12 @@
 export default {
-	layout: "post.njk",
-	permalink: "blog/{{ page.fileSlug }}/",
-	imageDir: "src/assets/img/",
-	tags: "posts",
-	image: {
-		source: "11tybundle-dev.png",
-		alt: "the number 11 as photographed by David Monje on Unsplash",
-	},
-	snow: "false",
-	confetti: "false",
+  layout: "post.njk",
+  permalink: "blog/{{ page.fileSlug }}/",
+  imageDir: "src/assets/img/",
+  tags: "posts",
+  image: {
+    source: "11tybundle-dev.png",
+    alt: "A cute graphic depicting the 11ty Bundle Dev logo",
+  },
+  snow: "false",
+  confetti: "false",
 };
