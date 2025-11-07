aef63ba8ffc3eaa76af3f10735f0767e9e6a7e8e
Bob Monsour <bob.monsour@gmail.com>
2025-11-07 15:23:13 -0800

    Added favicons to cat and author pages

--- src/_config/filters/index.js
+++ src/_config/filters/index.js
@@ -1,12 +1,13 @@
 import { isCurrentPage } from "./iscurrentpage.js";
 import {
-	formatItemDate,
-	formatPostDate,
-	formatFirehoseDate,
-	formatNumber,
+  formatItemDate,
+  formatPostDate,
+  formatFirehoseDate,
+  formatNumber,
 } from "./datesandnumbers.js";
 import { getBundleItems } from "./getbundleitems.js";
 import { getDescription } from "./getdescription.js";
+import { getFavicon } from "./getfavicon.js";
 import { getRSSlink } from "./getrsslink.js";
 import { plainDate } from "./datesandnumbers.js";
 import { postCountLabel } from "./postcountlabel.js";
@@ -15,24 +16,25 @@ import { postsInCategory } from "./postsincategory.js";
 import { readingTime } from "./readingtime.js";
 
 const filters = {
-	isCurrentPage,
-	formatItemDate,
-	formatPostDate,
-	formatFirehoseDate,
-	formatNumber,
-	getBundleItems,
-	getDescription,
-	getRSSlink,
-	plainDate,
-	postCountByAuthor,
-	postCountLabel,
-	postsByAuthor,
-	postsInCategory,
-	readingTime,
+  isCurrentPage,
+  formatItemDate,
+  formatPostDate,
+  formatFirehoseDate,
+  formatNumber,
+  getBundleItems,
+  getDescription,
+  getFavicon,
+  getRSSlink,
+  plainDate,
+  postCountByAuthor,
+  postCountLabel,
+  postsByAuthor,
+  postsInCategory,
+  readingTime,
 };
 
 export default (eleventyConfig) => {
-	return Object.keys(filters).forEach((filter) => {
-		eleventyConfig.addFilter(filter, filters[filter]);
-	});
+  return Object.keys(filters).forEach((filter) => {
+    eleventyConfig.addFilter(filter, filters[filter]);
+  });
 };
