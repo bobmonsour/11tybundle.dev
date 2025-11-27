6a5f48712455763976c2858e86a30c2135dd6108
Bob Monsour <bob.monsour@gmail.com>
2025-11-27 11:10:40 -0800

    Author icons working on home page

--- content/_config/filters/getwebandrssicons.js
+++ content/_config/filters/getwebandrssicons.js
@@ -3,7 +3,10 @@ import { getRSSLink } from "../filters/getrsslink.js";
 export const getWebIcon = async (link) => {
   const url = new URL(link);
   const siteUrl = url.origin;
-  return `<a href="${siteUrl}"><img src="/assets/img/globe.svg" alt="link to author's website" class="social-icon"></a>`;
+  return `<li><a href="${siteUrl}" class="🎈"><svg width="20" height="20" viewBox="0 0 24 24" aria-hidden="true">
+  <use xlink:href="#icon-globe"></use>
+  </svg><span class="visually-hidden">Website</span></a></li>`;
+  // return `<a href="${siteUrl}"><img src="/assets/img/globe.svg" alt="link to author's website" class="social-icon"></a>`;
 };
 
 export const getRSSIcon = async (link) => {
@@ -12,5 +15,10 @@ export const getRSSIcon = async (link) => {
   let rssLink = await getRSSLink(siteUrl);
   return rssLink === ""
     ? ""
-    : `<a href="${rssLink}"><img src="/assets/img/rss.svg" alt="link to author's rss feed" class="social-icon"></a>`;
+    : `<li><a href="${rssLink}" class="🎈"><svg width="20" height="20" viewBox="0 0 24 24" aria-hidden="true">
+        <use xlink:href="#icon-rss"></use>
+      </svg><span class="visually-hidden">RSS</span></a></li>`;
+  // return rssLink === ""
+  //   ? ""
+  //   : `<a href="${rssLink}"><img src="/assets/img/rss.svg" alt="link to author's rss feed" class="social-icon"></a>`;
 };
