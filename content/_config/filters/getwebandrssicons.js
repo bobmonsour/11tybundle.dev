1c33a683942271d10f928581bd966e845cbe5215
Bob Monsour <bob.monsour@gmail.com>
2025-11-30 16:53:10 -0800

    Misc comment cleanup

--- content/_config/filters/getwebandrssicons.js
+++ content/_config/filters/getwebandrssicons.js
@@ -6,7 +6,6 @@ export const getWebIcon = async (link) => {
   return `<li><a href="${siteUrl}" class="🎈"><svg width="20" height="20" viewBox="0 0 24 24" aria-hidden="true">
   <use xlink:href="#icon-globe"></use>
   </svg><span class="visually-hidden">Website</span></a></li>`;
-  // return `<a href="${siteUrl}"><img src="/assets/img/globe.svg" alt="link to author's website" class="social-icon"></a>`;
 };
 
 export const getRSSIcon = async (link) => {
@@ -18,7 +17,4 @@ export const getRSSIcon = async (link) => {
     : `<li><a href="${rssLink}" class="🎈"><svg width="20" height="20" viewBox="0 0 24 24" aria-hidden="true">
         <use xlink:href="#icon-rss"></use>
       </svg><span class="visually-hidden">RSS</span></a></li>`;
-  // return rssLink === ""
-  //   ? ""
-  //   : `<a href="${rssLink}"><img src="/assets/img/rss.svg" alt="link to author's rss feed" class="social-icon"></a>`;
 };
--- content/_config/filters/readingtime.js
+++ content/_config/filters/readingtime.js
@@ -12,28 +12,30 @@
 //	 displayed as "less than a minute"
 //
 // @param {String} text
+//
+// TODO: WILL NEED TO UPDATE THIS TO HANDLE NEW TEMPLATE STRUCTURE
 export const readingTime = (text) => {
-	var content = new String(text);
-	const speed = 240; // reading speed in words per minute
+  var content = new String(text);
+  const speed = 240; // reading speed in words per minute
 
-	// remove all html elements
-	var re = /(&lt;.*?&gt;)|(<[^>]+>)/gi;
-	var plain = content.replace(re, "");
+  // remove all html elements
+  var re = /(&lt;.*?&gt;)|(<[^>]+>)/gi;
+  var plain = content.replace(re, "");
 
-	// replace all newlines and 's with spaces
-	var plain = plain.replace(/\s+|'s/g, " ");
+  // replace all newlines and 's with spaces
+  var plain = plain.replace(/\s+|'s/g, " ");
 
-	// create array of all the words in the post & count them
-	var words = plain.split(" ");
-	var count = words.length;
+  // create array of all the words in the post & count them
+  var words = plain.split(" ");
+  var count = words.length;
 
-	// calculate the reading time
-	var readingTime = Math.round(count / speed);
-	if (readingTime === 0) {
-		return "Less than 1 minute to read";
-	} else if (readingTime === 1) {
-		return "1 minute to read";
-	} else {
-		return readingTime + " minutes to read";
-	}
+  // calculate the reading time
+  var readingTime = Math.round(count / speed);
+  if (readingTime === 0) {
+    return "Less than 1 minute to read";
+  } else if (readingTime === 1) {
+    return "1 minute to read";
+  } else {
+    return readingTime + " minutes to read";
+  }
 };
