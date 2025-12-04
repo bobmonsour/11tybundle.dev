c86a8f60cc7a26a8481525e60e0655bac5746cf0
Bob Monsour <bob.monsour@gmail.com>
2025-12-04 09:21:11 -0800

    Created new filter

--- content/_config/filters/singlecomma.js
+++ content/_config/filters/singlecomma.js
@@ -1,5 +1,6 @@
 // Take a number an, if more than 999, insert a single comma
-// before the last three digits
+// before the last three digits; only handles commas for thousands
+// Example: 1234 becomes "1,234", 123 becomes "123"
 export const singleComma = (input) => {
   const s = String(input).replace(/,/g, "");
   if (s.length <= 3) return s;
