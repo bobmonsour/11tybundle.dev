5a0aaa32ac823c7fc927178891bbb1407c477a20
Bob Monsour <bob.monsour@gmail.com>
2025-12-10 09:23:49 -0800

    Integrated new search styling

---
+++ public/js/search.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", () => {
+  new PagefindUI({
+    element: "#search",
+    translations: {
+      placeholder: "Type / to search",
+      zero_results: "Count not find [SEARCH_TERM]",
+    },
+    showSubResults: true,
+    showSubResults: true,
+    showImages: false,
+    showEmptyFilters: false,
+    showSubResults: true,
+    excerptLength: 100,
+    pageSize: 5,
+  });
+
+  let focusTriggeredBySlash = false;
+  // Flag to track focus triggered by '/'
+
+  // Add event listener for the '/' key
+  document.addEventListener("keydown", (e) => {
+    // Check if the '/' key was pressed and no input or textarea is focused
+    if (e.key === "/" && !e.target.matches("input, textarea")) {
+      e.preventDefault();
+      // Prevent the default browser behavior
+      const searchInput = document.querySelector(
+        "input.pagefind-ui__search-input"
+      );
+      if (searchInput) {
+        focusTriggeredBySlash = true;
+        // Set the flag
+        searchInput.focus();
+        // Move focus to the search box
+      }
+    }
+  });
+
+  // Add event listener for the focus event on the input element
+  const searchInput = document.querySelector("input.pagefind-ui__search-input");
+  if (searchInput) {
+    searchInput.addEventListener("focus", () => {
+      if (focusTriggeredBySlash) {
+        fathom.trackEvent("search with keyboard shortcut");
+        focusTriggeredBySlash = false;
+        // Reset the flag
+      } else {
+        fathom.trackEvent("search without keyboard shortcut");
+      }
+    });
+  }
+});
