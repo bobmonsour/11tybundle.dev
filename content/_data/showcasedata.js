1a1c520ee338b8a21ef42f24f33d28be1d5dd6a3
Bob Monsour <bob.monsour@gmail.com>
2026-01-03 15:04:17 -0800

    Showcase is alive

--- content/_data/showcasedata.js
+++ content/_data/showcasedata.js
@@ -1,7 +1,13 @@
-const localData = await import(
-  "/Users/Bob/Dropbox/Docs/Sites/11tybundle/11tybundledb/showcase-data.json",
-  {
-    with: { type: "json" },
-  }
-);
-export const showcasedata = localData;
+export default async function () {
+  const localData = await import(
+    "/Users/Bob/Dropbox/Docs/Sites/11tybundle/11tybundledb/showcase-data.json",
+    {
+      with: { type: "json" },
+    }
+  );
+
+  const sites = localData.default;
+  const siteCount = sites.length;
+  console.log(`Showcase site count: ${siteCount}`);
+  return { sites, siteCount };
+}
