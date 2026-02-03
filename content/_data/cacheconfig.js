7a4136d0fbd405ddc7608c0c42144f1feed288c7
Bob Monsour <bob.monsour@gmail.com>
2026-02-03 09:58:38 -0800

    Cache starter info for 1 week

--- content/_data/cacheconfig.js
+++ content/_data/cacheconfig.js
@@ -18,7 +18,7 @@ export const cacheDuration = {
   rssLinkHtml: "1y",
   socialHtml: "1y",
   socialLinks: "1y",
-  starters: "0s",
+  starters: "1w",
 };
 
 // Timeout settings for fetch operations, in milliseconds
