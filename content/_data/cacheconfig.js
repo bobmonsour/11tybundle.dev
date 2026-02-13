2fed821409e929a66b748cb331de25a8909b9ca2
Bob Monsour <bob.monsour@gmail.com>
2026-02-12 17:47:01 -0800

    Reduced cache duration for starters to 1d

--- content/_data/cacheconfig.js
+++ content/_data/cacheconfig.js
@@ -18,7 +18,7 @@ export const cacheDuration = {
   rssLinkHtml: "1y",
   socialHtml: "1y",
   socialLinks: "1y",
-  starters: "1w",
+  starters: "1d",
 };
 
 // Timeout settings for fetch operations, in milliseconds
